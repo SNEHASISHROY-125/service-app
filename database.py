449664d58c4661ca0ac05f9cd1881778c09162b1
snehasish <rsnhasish125@gmail.com>
2024-09-22 21:58:59 +0530

    m

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine, Column, Integer, String, MetaData, Table
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "sqlite:///./test.db"
+DATABASE_URL = "sqlite:////home/site/wwwroot/sqlite.db"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
