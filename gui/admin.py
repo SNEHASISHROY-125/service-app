9a4726d9d6b4190248980cd54247b01942a15aed
SNEHASISH ROY <rsnehasish125@gmail.com>
2024-11-11 22:38:55 +0530

    performance+

--- gui/admin.py
+++ gui/admin.py
@@ -128,7 +128,7 @@ MDScreenManager:
                         opacity: .4
                         #radius: dp(24)
 
-                    ScrollView:
+                    RefreshScrollView_Complaints:
                         do_scroll_x: False
                         MDList:
                             id: complaints_list
@@ -156,7 +156,7 @@ MDScreenManager:
                                     icon: "chevron-right"
 '''
 
-global server_url 
+global server_url
 server_url = "https://chat-app.fudemy.me/" # server url
 
 class Item(OneLineAvatarListItem):
@@ -193,9 +193,6 @@ class RefreshScrollView(PullToRefreshBehavior, ScrollView):
     global _APP
 
     def refresh(self):
-        # self.refreshing = True
-        # self.spinner.opacity = 1  # Show the spinner
-        # self.spinner.active = True  # Start the spinner
         print("Pulled down to refresh!")
         _modal.open()
         # Add your refresh logic here
@@ -208,21 +205,27 @@ class RefreshScrollView(PullToRefreshBehavior, ScrollView):
         except Exception as e:
             print(e)
 
-    # def update_list(self, dt):
-    #     self.refreshing = False
-    #     try:
-    #         _APP.render_engineers()
-    #     except Exception as e:
-    #         print(e)
-        # print(_APP)
-        # _modal.dismiss()
-        # self.spinner.opacity = 0  # Hide the spinner
-        # self.spinner.active = False  # Stop the spinner
+class RefreshScrollView_Complaints(PullToRefreshBehavior, ScrollView):
+    refreshing = BooleanProperty(False)
+    spinner = ObjectProperty(None)
+    global _modal
+    global _APP
+
+    def refresh(self):
+        print("Pulled down to refresh!")
+        _modal.open()
+        # Add your refresh logic here
+        Clock.schedule_once(_APP.render_complaints, .3)  # Simulate a delay
+        # threading.Thread(target=self.run_background_task).start()  # Run the background task in a separate thread
+
 
 class AdminDashboardApp(MDApp):
     s_ = source_
     dialog = None
+    _dialog = None
     def build(self):
+        self.theme_cls.primary_palette = "Blue"
+        self.theme_cls.primary_hue = "A700" 
         return Builder.load_string(KV)
     
     def _init_loading_widget(self):
@@ -252,116 +255,115 @@ class AdminDashboardApp(MDApp):
         # _modal_issue.bind(on_pre_dismiss=on_pre_dismiss)
 
     
-    def render_complaints(self):
+    def render_complaints(self, *args):
+        # self.root.ids.refresh_scroll.refreshing = False
+        # self.root.ids.refresh_scroll.spinner.active = False  # Stop the spinner
+        # self.root.ids.refresh_scroll.spinner.opacity = 0  # Hide the spinner
+        # make query to get all engineers
         def _query():
             import requests
             from kivy.clock import Clock
             from kivymd.toast import toast
-            global _issue
+            import time
+            server_url = "http://chat-app.fudemy.me/"
+            global _complaints
             try:
-                _issue = requests.get(url=server_url+"get_all", params={"table_name": "issues"}).json()
-                print(_issue)
+                print(server_url)
+                time.sleep(1) # simulate a delay | blocks main thread
+                _complaints = requests.get(url=server_url+"get_all", params={"table_name": "issues"}).json()
+                print(_complaints)
             except Exception as e:
                 Clock.schedule_once(lambda dt: toast('You are offlline'),0.5)
                 Clock.schedule_once(lambda dt: _modal.dismiss() ,1)
                 return
             # if response contains issues ->
-            if _issue:
-                threading.Thread(target=Clock.schedule_once(_add_complaints,0.1)).start()
-
-
-        def _add_complaints(dt):
-            # appointments = _appointments_data
-            # the root MDList to add appointments to
-            global _issue
+            if _complaints:
+                Clock.schedule_once(lambda x:_add_complaints(), 0.1)
+        def _add_complaints():
             global _modal
-            def bind_list_item(x):
-                # print(x.text)
-                # scroll_view = ScrollView(
-                #     do_scroll_x= False
-                #     )
-                # list_item = TwoLineAvatarIconListItem(text=x['issue'], secondary_text=x['complaintid'])
-                # scroll_view.add_widget(list_item)
-                # _modal_issue.add_widget(scroll_view)
-
-                def not_none(x): 
+            global _dialog
+            def not_none(x): 
                     if not x :
                         print(x)
                         return 'not-availale'
                     else: return x
-                # dialog
-                m_  =   MDDialog(
-                    title=x['status'],
-                    type="simple",
-                    items=[
-                        Item(text=not_none(x['issue']), theme_text_color="Custom", text_color=self.theme_cls.primary_color, source="https://cdn.iconscout.com/icon/free/png-256/doctor-1851563-1569282.png"),
-                        Item(text=not_none(x['complaintid']), theme_text_color="Custom", text_color=self.theme_cls.primary_color, source="https://cdn.iconscout.com/icon/free/png-256/doctor-1851563-1569282.png"),
-                        Item(text=not_none(x['status']), theme_text_color="Custom", text_color=self.theme_cls.primary_color, source="https://img.icons8.com/color/240/ok--v1.png"),
-                        Item(text=not_none(x['name']), theme_text_color="Custom", text_color=self.theme_cls.primary_color, source="https://cdn.iconscout.com/icon/free/png-256/doctor-1851563-1569282.png"),
-                        Item(text=not_none(str(x['phone'])), theme_text_color="Custom", text_color=self.theme_cls.primary_color, source="https://cdn.iconscout.com/icon/free/png-256/doctor-1851563-1569282.png"),
-                        Item(text=not_none(x['user_id']), theme_text_color="Custom", text_color=self.theme_cls.primary_color, source="https://cdn.iconscout.com/icon/free/png-256/doctor-1851563-1569282.png"),
-                        Item(text=not_none(x['esttime']), theme_text_color="Custom", text_color=self.theme_cls.primary_color, source="https://cdn.iconscout.com/icon/free/png-256/doctor-1851563-1569282.png"),
-                        Item(text=not_none(x['location']), theme_text_color="Custom", text_color=self.theme_cls.primary_color, source="https://cdn.iconscout.com/icon/free/png-256/doctor-1851563-1569282.png"),
-                        Item(text=not_none(x['payments_receipt']), theme_text_color="Custom", text_color=self.theme_cls.primary_color, source="https://cdn.iconscout.com/icon/free/png-256/doctor-1851563-1569282.png"),
-                    ],
-                    buttons=[
-                        MDFlatButton(
-                            text="CLOSE",
-                            theme_text_color="Custom",
-                            text_color=self.theme_cls.primary_color,
-                            on_release=lambda x: m_.dismiss()
-                        ),
-                    ],
-                )
-                
-                #
-                global _modal
-                m_.bind(on_open=lambda x:_modal.dismiss())
-                m_.bind(on_pre_dismiss=lambda x : m_.clear_widgets())
-                # m_.bind(on_pre_dismiss=lambda x: clr(m=m_))
-                m_.open()
-
-            appointments_list = self.root.get_screen("admin_dashboard").ids.complaints_list
-            # appointments = {'complaint-id': '#56608' ,'attd-name': 'NAME_', 'time': '12:00 PM', 'status': 'pending'}
-            # remove all widgets from the list
-            appointments_list.clear_widgets()
-            for i in _issue['data']:
-                print(i)
-                # icon
-                if i['status'] == 'open': icon_ = "clock"
-                elif i['status'] == 'closed': icon_ = "check-circle-outline"
-                elif i['status'] == 'canceled': icon_ = "close-circle-outline"
-                _issue_widget = TwoLineAvatarIconListItem(
+            
+            def _show_dialog(_issue_data):
+                self._dialog.title = f"Complaint {_issue_data['complaintid']}"
+                _items_list = self._dialog.children[0].children[2].children[0].children
+                print(self._dialog.children[0].children[2].children[0].children[0].source)
+                _payments_receipt = _items_list[0]
+                _location = _items_list[1]
+                _esttime = _items_list[2]
+                _user_id = _items_list[3]
+                _phone = _items_list[4]
+                _name = _items_list[5]
+                _status = _items_list[6]
+                _complaintid = _items_list[7]
+                _issue = _items_list[8]
+                #  set status icon
+                _status.source="https://cdn.iconscout.com/icon/free/png-256/doctor-1851563-1569282.png" if _issue_data['status'] == 'open' else "https://img.icons8.com/color/240/ok--v1.png" if _issue_data['status'] == 'closed' else "https://img.icons8.com/color/240/close-sign.png"
+                # set values
+                _payments_receipt.text = not_none(str(_issue_data['payments_receipt']))
+                _location.text = not_none(_issue_data['location'])
+                _esttime.text = not_none(_issue_data['esttime'])
+                _user_id.text = not_none(_issue_data['user_id'])
+                _phone.text = not_none(str(_issue_data['phone']))
+                _name.text = not_none(_issue_data['name'])
+                _status.text = not_none(_issue_data['status'])
+                _complaintid.text = not_none(str(_issue_data['complaintid']))
+                _issue.text = not_none(_issue_data['issue'])
+                # print(_payments_receipt,'\n',_location.text,'\n',_esttime,'\n',_user_id,'\n',_phone,'\n',_name,'\n',_status,'\n',_complaintid,'\n',_issue)
+                # release resorces
+                for i in [_payments_receipt, _location, _esttime, _user_id, _phone, _name, _status, _complaintid, _issue]:
+                    i = None
+                    # i = None
+                # print(_dialog.children[0].children[2].children[0].children[1].text)
+                self._dialog.open()
+            # 
+            engineers_list = self.root.get_screen("admin_dashboard").ids.complaints_list
+            print(engineers_list.children)
+            #
+            # engineers_list.clear_widgets()
+            # print(engineers_list.children)
+            # Logic to match list items with the engineers
+            if len(engineers_list.children) < len(_complaints['data']):
+                for i in range(len(_complaints['data']) - len(engineers_list.children)):
+                    engineers_list.add_widget(TwoLineAvatarIconListItem(
                         IconLeftWidget(
-                            icon=icon_,
-                            theme_text_color="Custom",
-                            text_color=self.theme_cls.primary_color
                         ),
                         IconRightWidget(
-                            icon="chevron-right",
-                            theme_text_color="Custom",
-                            text_color=self.theme_cls.primary_color,
-                            on_press=lambda x: threading.Thread(target=_modal.open()).start(),
-                            on_release=lambda x,i=i: bind_list_item(i),
                         ),
-                        text= i['issue'],
-                        theme_text_color="Custom",
-                        text_color = self.theme_cls.primary_color,
-                        secondary_text= i['complaintid'],
+                        text="Two-line item with avatar",secondary_text="Secondary text here",theme_text_color="Custom",text_color=self.theme_cls.primary_color
+                        )
                     )
-                # bind the on_press event
-                _issue_widget.bind(on_release=lambda x: print(x.text,x))
-                # add the widget to the list
-                appointments_list.add_widget(_issue_widget, index=0)
-            
-            # relese-resources
-            appointments_list = None
-            # _issue = None
-            # print("Appointments added\n",res_,phone_)
-            Clock.schedule_once(lambda dt: _modal.dismiss() ,1)
-        # 
-        threading.Thread(target=_query).start()
+                    print('added new to list')
 
+            for i in engineers_list.children:
+                print(engineers_list.children.index(i))
+                i.theme_text_color = "Custom"
+                i.text_color = self.theme_cls.primary_color
+                print(_complaints['data'][engineers_list.children.index(i)])
+                i.text = _complaints['data'][engineers_list.children.index(i)]['issue']
+                i.secondary_text = str( _complaints['data'][engineers_list.children.index(i)]['complaintid'])
+                #
+                rightwidget = i.children[0].children
+                rightwidget[0].icon = "chevron-right"
+                rightwidget[0].theme_text_color = "Custom"
+                rightwidget[0].text_color = self.theme_cls.primary_color
+                rightwidget[0].on_release = lambda data=_complaints['data'][engineers_list.children.index(i)] : _show_dialog(data)
+
+                #
+                leftwidget = i.children[1].children
+                leftwidget[0].icon = "clock" if _complaints['data'][engineers_list.children.index(i)]['status'] == 'open' else "check-circle-outline" if _complaints['data'][engineers_list.children.index(i)]['status'] == 'closed' else "close-circle-outline"
+                # print(i.text,i.secondary_text,i.children[1].children)
+
+            #     engineers_list.add_widget(OneLineListItem(text=f"Item {i}"))
+
+            _modal.dismiss()
+        # _query()
+        Clock.schedule_once(lambda x:threading.Thread(target=_query).start(),0.5)
+    
     # def render_engineers(self):
     #     # self.root.get_screen("admin_dashboard").ids.engineers_list
     #     def _query():
@@ -568,6 +570,30 @@ class AdminDashboardApp(MDApp):
         _modal.open()
         self.render_complaints()
         # 3
+        self._dialog = MDDialog(
+                    title='status',
+                    type="simple",
+                    items=[
+                        Item(text='issue', theme_text_color="Custom", text_color=self.theme_cls.primary_color, source="https://cdn.iconscout.com/icon/free/png-256/doctor-1851563-1569282.png"),
+                        Item(text='complaintid', theme_text_color="Custom", text_color=self.theme_cls.primary_color, source="https://cdn.iconscout.com/icon/free/png-256/doctor-1851563-1569282.png"),
+                        Item(text='status', theme_text_color="Custom", text_color=self.theme_cls.primary_color, source="https://img.icons8.com/color/240/ok--v1.png"),
+                        Item(text='name', theme_text_color="Custom", text_color=self.theme_cls.primary_color, source="https://cdn.iconscout.com/icon/free/png-256/doctor-1851563-1569282.png"),
+                        Item(text='phone', theme_text_color="Custom", text_color=self.theme_cls.primary_color, source="https://cdn.iconscout.com/icon/free/png-256/doctor-1851563-1569282.png"),
+                        Item(text='user_id', theme_text_color="Custom", text_color=self.theme_cls.primary_color, source="https://cdn.iconscout.com/icon/free/png-256/doctor-1851563-1569282.png"),
+                        Item(text='esttime', theme_text_color="Custom", text_color=self.theme_cls.primary_color, source="https://cdn.iconscout.com/icon/free/png-256/doctor-1851563-1569282.png"),
+                        Item(text='location', theme_text_color="Custom", text_color=self.theme_cls.primary_color, source="https://cdn.iconscout.com/icon/free/png-256/doctor-1851563-1569282.png"),
+                        Item(text='payments_receipt', theme_text_color="Custom", text_color=self.theme_cls.primary_color, source="https://cdn.iconscout.com/icon/free/png-256/doctor-1851563-1569282.png"),
+                    ],
+                    buttons=[
+                        MDFlatButton(
+                            text="CLOSE",
+                            theme_text_color="Custom",
+                            text_color=self.theme_cls.primary_color,
+                            on_release=lambda x: self._dialog.dismiss()
+                        ),
+                    ],
+                )
+        self._dialog.open()
         # Force garbage collection
         gc.collect()
     def on_stop(self):
@@ -588,10 +614,12 @@ class AdminDashboardApp(MDApp):
             import requests
             from kivy.clock import Clock
             from kivymd.toast import toast
+            import time
             server_url = "http://chat-app.fudemy.me/"
             global _engineers
             try:
                 print(server_url)
+                time.sleep(1) # simulate a delay | blocks main thread
                 _engineers = requests.get(url=server_url+"get_all", params={"table_name": "service_engineers"}).json()
                 print(_engineers)
             except Exception as e:
